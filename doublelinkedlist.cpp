388febe65408306b9aa045c05a6df924f5dba73d
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-04-28 14:28:04 +0700

    implementasi logika sisip node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -31,3 +31,18 @@ void addNode()
         Node *newNode = new Node();
         newNode->noMhs = nim;
     }
+
+    if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            newNode->next = START;
+            if (START != NULL)
+                START->prev = newNode;
+            newNode->prev = NULL;
+            START = newNode;
+            return;
+        }
