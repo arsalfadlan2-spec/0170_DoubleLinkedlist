e51f1fec35ac137e50d44acdb86c132bf865b88f
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-04-28 14:28:36 +0700

    menambahkan logika pencarian

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -46,3 +46,15 @@ void addNode()
             START = newNode;
             return;
         }
+
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
